2ab78e83fc703a927ae54d157c76a83a65114776
dev-wangliugen <253192463@qq.com>
2017-05-09 15:00:30 +0800

    java countcoins

---
+++ JavaPractice/src/main/java/CountCoins.java
@@ -0,0 +1,13 @@
+/**
+ * Created by wangliugen on 2017/5/10.
+ */
+public class CountCoins {
+    private Integer quarters = 25;
+    private Integer dimes = 10;
+    private Integer nickels = 5;
+    private Integer penneis = 1;
+    private Integer times = 0;
+    public  void  testCountCoins() {
+
+    }
+}
