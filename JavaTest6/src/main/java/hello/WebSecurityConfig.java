1413284ce7973b8b35e2ce363ce569ecb9ad046d
hoggenw <253192463@qq.com>
2017-06-01 08:30:07 +0800

    config test6

---
+++ JavaTest6/src/main/java/hello/WebSecurityConfig.java
@@ -0,0 +1,8 @@
+package hello;
+
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSecurityConfig  extends WebSecurityConfigurerAdapter{
+}
