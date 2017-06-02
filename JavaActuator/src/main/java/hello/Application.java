6bde2f06dbf7d1c1ce7bf13c86b03d3d761ea3c0
hoggenw <253192463@qq.com>
2017-06-02 10:25:36 +0800

    actuator finish

--- JavaActuator/src/main/java/hello/Application.java
+++ JavaActuator/src/main/java/hello/Application.java
@@ -2,6 +2,14 @@ package hello;
 
 //actuator是spring boot提供的对应用系统的自省和监控的集成功能，可以对应用系统进行配置查看、相关功能统计等。
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class,args);
+    }
+
 }
