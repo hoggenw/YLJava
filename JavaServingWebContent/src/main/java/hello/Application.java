0b41863a3b3ce9b4c1ed3b1116fbebdc3cbf4d8f
hoggenw <253192463@qq.com>
2017-06-05 15:16:05 +0800

    async

--- JavaServingWebContent/src/main/java/hello/Application.java
+++ JavaServingWebContent/src/main/java/hello/Application.java
@@ -2,6 +2,9 @@ package hello;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
 public class Application {
@@ -10,5 +13,14 @@ public class Application {
         SpringApplication.run(Application.class, args);
     }
 
+    public Executor getAsyncExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(2);
+        executor.setMaxPoolSize(2);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("GithubLookup-");
+        executor.initialize();
+        return executor;
+    }
 }
 
--- JavaServingWebContent/src/main/java/hello/GreetingController.java
+++ JavaServingWebContent/src/main/java/hello/GreetingController.java
@@ -4,12 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-
-@Controller
-public class GreetingController {
-    @RequestMapping("/greeting")
-    public String greeting(@RequestParam() String name, Model model) {
-        model.addAttribute("name",name);
-        return "greeting";
-    }
-}
+//
+//@Controller
+//public class GreetingController {
+//    @RequestMapping("/greeting")
+//    public String greeting(@RequestParam() String name, Model model) {
+//        model.addAttribute("name",name);
+//        return "greeting";
+//    }
+//}
