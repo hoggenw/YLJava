0329a5a18775f877daf48b1e32ea16bf4247a62d
hoggenw <253192463@qq.com>
2018-09-19 15:07:00 +0800

    no message

---
+++ eclipse-workspace/ServetTest/src/hoggen/wang/HelloServlet.java
@@ -0,0 +1,40 @@
+package hoggen.wang;
+
+import java.io.IOException;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/hello")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public HelloServlet() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html;charset=utf-8");
+		// 想浏览器输出
+		response.getWriter().write("当前时间为:  " + new Date());
+		// response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+}
