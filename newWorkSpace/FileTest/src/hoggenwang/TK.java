caaa4f130e952772bed8531d58caa326d1c552b1
hoggenw <253192463@qq.com>
2018-10-05 20:17:19 +0800

    no message

---
+++ newWorkSpace/FileTest/src/hoggenwang/TK.java
@@ -0,0 +1,19 @@
+package hoggenwang;
+
+import org.junit.Test;
+
+public class TK<T, K> {
+
+	public T save(T t, K k) {
+		return t;
+	}
+
+	@Test
+	public void testTK() {
+
+		// FileModel model = save(new FileModel(),new String());
+		// TK<FileModel,String> tk = new TK();
+
+	}
+
+}
