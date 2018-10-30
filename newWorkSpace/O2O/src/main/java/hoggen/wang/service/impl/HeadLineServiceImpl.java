3ab8a17d9397c687985d16e94f8fc1d9a2538bb3
hoggenw <253192463@qq.com>
2018-10-30 10:52:14 +0800

    no message

--- newWorkSpace/O2O/src/main/java/hoggen/wang/service/impl/HeadLineServiceImpl.java
+++ newWorkSpace/O2O/src/main/java/hoggen/wang/service/impl/HeadLineServiceImpl.java
@@ -1,12 +1,18 @@
 package hoggen.wang.service.impl;
 
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import hoggen.wang.cache.JedisUtil;
 import hoggen.wang.dao.HeadLineDao;
 import hoggen.wang.dto.HeadLineExecution;
 import hoggen.wang.entity.HeadLine;
@@ -17,11 +23,39 @@ public class HeadLineServiceImpl implements HeadLineService {
 
 	@Autowired
 	private HeadLineDao headLineDao;
+	@Autowired
+	private JedisUtil.Keys jedisKeys;
+	@Autowired
+	private JedisUtil.Strings jedisStrings;
+
+	private static String HEADERLINELIST = "headerlineList";
 
 	@Override
+	@Transactional
 	public List<HeadLine> getHeadLineList(HeadLine headLineCondition) throws IOException {
 		// TODO Auto-generated method stub
-		return headLineDao.queryHeadLine(headLineCondition);
+		String key = HEADERLINELIST;
+		List<HeadLine> headLines = null;
+		ObjectMapper mapper = new ObjectMapper();
+		if (headLineCondition != null && headLineCondition.getEnableStatus() != null) {
+			key = key + "_" + headLineCondition.getEnableStatus();
+		}
+		if (!jedisKeys.exists(key)) {
+			headLines = headLineDao.queryHeadLine(headLineCondition);
+			String jsonString = mapper.writeValueAsString(headLines);
+			if (jsonString != null) {
+				jedisStrings.set(key, jsonString);
+			}
+		} else {
+			String jsonString = jedisStrings.get(key);
+
+			JavaType javaType = mapper.getTypeFactory().constructParametricType(ArrayList.class, HeadLine.class);
+
+			headLines = mapper.readValue(jsonString, javaType);
+
+		}
+
+		return headLines;
 	}
 
 	@Override
