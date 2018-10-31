5ac90aaf0baa68a95f78763da557fe2dd1b8aa83
hoggenw <253192463@qq.com>
2018-10-31 14:20:29 +0800

    移除不需要代码

--- stsWorkSpace/O2O0/src/main/java/service/CacheService.java
+++
@@ -1,9 +0,0 @@
-package hoggen.wang.service;
-
-public interface CacheService {
-	/**
-	 * @注释 依据key前缀匹配该模式下的所有key-value
-	 */
-
-	void removeFromCache(String keyPrefix);
-}
--- stsWorkSpace/O2O0/src/main/java/util/PageCalculatorUtil.java
+++
@@ -1,8 +0,0 @@
-package hoggen.wang.util;
-
-public class PageCalculatorUtil {
-	public static int calculatorRowIndex(int pageIndex, int pagesize) {
-		return (pageIndex > 0) ? (pageIndex - 1) * pagesize : 0;
-	}
-
-}
