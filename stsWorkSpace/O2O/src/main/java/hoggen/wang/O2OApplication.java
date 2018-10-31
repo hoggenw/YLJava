11d66eaf76cd56e42115170ba56937b078dc1687
hoggenw <253192463@qq.com>
2018-10-31 14:17:36 +0800

    no message

---
+++ stsWorkSpace/O2O/src/main/java/hoggen/wang/O2OApplication.java
@@ -0,0 +1,13 @@
+package hoggen.wang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+//@ComponentScan(basePackages = { "hoggen.wang" })
+public class O2OApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(O2OApplication.class, args);
+	}
+}
