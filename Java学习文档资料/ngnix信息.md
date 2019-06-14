05c75825b4df198a98a3a9f01c002dc2b4ffc018
hoggenw <253192463@qq.com>
2019-06-14 11:07:14 +0800

    no message

---
+++ Java学习文档资料/ngnix信息.md
@@ -0,0 +1,8 @@
+nginx -s quit 退出
+
+nginx -s reload 重新加载
+
+nginx -t 测试nginx.conf配置
+
+sudo nginx 启动nginx
+
---
+++ idea/sublimation/src/main/java/com/hoggen/sublimation/SublimationApplication.java
@@ -0,0 +1,13 @@
+package com.hoggen.sublimation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SublimationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SublimationApplication.class, args);
+	}
+
+}
