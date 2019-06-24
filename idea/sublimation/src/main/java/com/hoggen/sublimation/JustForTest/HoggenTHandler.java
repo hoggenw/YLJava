5e21b0fa74b3458932baf4048cdbc66f4e10eeed
hoggenw <253192463@qq.com>
2019-06-24 17:22:10 +0800

    no message

---
+++ idea/sublimation/src/main/java/com/hoggen/sublimation/JustForTest/HoggenTHandler.java
@@ -0,0 +1,80 @@
+package com.hoggen.sublimation.Controller;
+
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.channel.*;
+
+public class HoggenTHandler extends SimpleChannelHandler {
+
+    @Override
+    public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        System.out.println("channelClosed");
+        super.channelClosed(ctx, e);
+    }
+
+    @Override
+    public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        System.out.println("channelConnected");
+        super.channelConnected(ctx, e);
+    }
+
+
+    @Override
+    public void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        System.out.println("channelDisconnected");
+        super.channelDisconnected(ctx, e);
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+        System.out.println("exceptionCaught");
+        super.exceptionCaught(ctx, e);
+    }
+
+    @Override
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+        System.out.println("messageReceived");
+       // ChannelBuffer messager = (ChannelBuffer) e.getMessage();
+        String messge = (String) e.getMessage();
+        System.out.println(messge);
+       // System.out.println(e.getMessage());
+
+        super.messageReceived(ctx, e);
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
