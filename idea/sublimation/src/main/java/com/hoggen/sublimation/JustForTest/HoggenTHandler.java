bbce5f80cebb621a2740770509bab25b392e0c21
hoggenw <253192463@qq.com>
2019-06-25 10:15:22 +0800

    no message

--- idea/sublimation/src/main/java/com/hoggen/sublimation/JustForTest/HoggenTHandler.java
+++ idea/sublimation/src/main/java/com/hoggen/sublimation/JustForTest/HoggenTHandler.java
@@ -1,85 +1,87 @@
-package com.hoggen.sublimation.JustForTest;
-
-import org.jboss.netty.buffer.ChannelBuffer;
-import org.jboss.netty.buffer.ChannelBuffers;
-import org.jboss.netty.channel.*;
-
-public class HoggenTHandler extends SimpleChannelHandler {
-
-    @Override
-    public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
-        System.out.println("channelClosed");
-        super.channelClosed(ctx, e);
-    }
-
-    @Override
-    public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
-        System.out.println("channelConnected");
-        super.channelConnected(ctx, e);
-    }
-
-
-    @Override
-    public void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
-        System.out.println("channelDisconnected");
-        super.channelDisconnected(ctx, e);
-    }
-
-    @Override
-    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
-        System.out.println("exceptionCaught");
-        super.exceptionCaught(ctx, e);
-    }
-
-    @Override
-    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
-        System.out.println("messageReceived");
-       // ChannelBuffer messager = (ChannelBuffer) e.getMessage();
-        String messge = (String) e.getMessage();
-        System.out.println(messge);
-       // System.out.println(e.getMessage());
-
-//        ChannelBuffer copiedBuffer = ChannelBuffers.copiedBuffer(("host: " + messge).getBytes());
-
-        ctx.getChannel().write("host:  " + messge);
-
-        super.messageReceived(ctx, e);
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//package com.hoggen.sublimation.JustForTest;
+//
+//
+////netty 3
+//import org.jboss.netty.buffer.ChannelBuffer;
+//import org.jboss.netty.buffer.ChannelBuffers;
+//import org.jboss.netty.channel.*;
+//
+//public class HoggenTHandler extends SimpleChannelHandler {
+//
+//    @Override
+//    public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+//        System.out.println("channelClosed");
+//        super.channelClosed(ctx, e);
+//    }
+//
+//    @Override
+//    public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+//        System.out.println("channelConnected");
+//        super.channelConnected(ctx, e);
+//    }
+//
+//
+//    @Override
+//    public void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+//        System.out.println("channelDisconnected");
+//        super.channelDisconnected(ctx, e);
+//    }
+//
+//    @Override
+//    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+//        System.out.println("exceptionCaught");
+//        super.exceptionCaught(ctx, e);
+//    }
+//
+//    @Override
+//    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+//        System.out.println("messageReceived");
+//       // ChannelBuffer messager = (ChannelBuffer) e.getMessage();
+//        String messge = (String) e.getMessage();
+//        System.out.println(messge);
+//       // System.out.println(e.getMessage());
+//
+////        ChannelBuffer copiedBuffer = ChannelBuffers.copiedBuffer(("host: " + messge).getBytes());
+//
+//        ctx.getChannel().write("host:  " + messge);
+//
+//        super.messageReceived(ctx, e);
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
--- idea/sublimation/src/main/java/com/hoggen/sublimation/JustForTest/ServerTest.java
+++ idea/sublimation/src/main/java/com/hoggen/sublimation/JustForTest/ServerTest.java
@@ -1,78 +1,81 @@
-package com.hoggen.sublimation.JustForTest;
-
-import org.jboss.netty.bootstrap.ServerBootstrap;
-import org.jboss.netty.channel.ChannelPipeline;
-import org.jboss.netty.channel.ChannelPipelineFactory;
-import org.jboss.netty.channel.Channels;
-import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
-import org.jboss.netty.handler.codec.string.StringDecoder;
-import org.jboss.netty.handler.codec.string.StringEncoder;
-
-import java.net.InetSocketAddress;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-public class ServerTest {
-
-    public  static void main(String[] args){
-        //服务类
-        ServerBootstrap bootstrap = new ServerBootstrap();
-        //线程池
-        ExecutorService boss = Executors.newCachedThreadPool();
-        ExecutorService worker = Executors.newCachedThreadPool();
-
-        //设置niosocket工厂
-        bootstrap.setFactory(new NioServerSocketChannelFactory(boss,worker));
-        //设置管道的工厂
-
-        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
-            @Override
-            public ChannelPipeline getPipeline() throws Exception {
-               ChannelPipeline pipeline =  Channels.pipeline();
-               //数据解析
-               pipeline.addLast("decoder",new StringDecoder());
-               //
-                pipeline.addLast("encoder",new StringEncoder());
-                //
-               pipeline.addLast("hoggenT",new HoggenTHandler());
-                return pipeline;
-            }
-        });
-
-
-        bootstrap.bind(new InetSocketAddress(8099));
-
-        System.out.println("start");
-
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+//package com.hoggen.sublimation.JustForTest;
+//
+//
+////netty 3
+//
+//import org.jboss.netty.bootstrap.ServerBootstrap;
+//import org.jboss.netty.channel.ChannelPipeline;
+//import org.jboss.netty.channel.ChannelPipelineFactory;
+//import org.jboss.netty.channel.Channels;
+//import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+//import org.jboss.netty.handler.codec.string.StringDecoder;
+//import org.jboss.netty.handler.codec.string.StringEncoder;
+//
+//import java.net.InetSocketAddress;
+//import java.util.concurrent.ExecutorService;
+//import java.util.concurrent.Executors;
+//
+//public class ServerTest {
+//
+//    public  static void main(String[] args){
+//        //服务类
+//        ServerBootstrap bootstrap = new ServerBootstrap();
+//        //线程池
+//        ExecutorService boss = Executors.newCachedThreadPool();
+//        ExecutorService worker = Executors.newCachedThreadPool();
+//
+//        //设置niosocket工厂
+//        bootstrap.setFactory(new NioServerSocketChannelFactory(boss,worker));
+//        //设置管道的工厂
+//
+//        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+//            @Override
+//            public ChannelPipeline getPipeline() throws Exception {
+//               ChannelPipeline pipeline =  Channels.pipeline();
+//               //数据解析
+//               pipeline.addLast("decoder",new StringDecoder());
+//               //
+//                pipeline.addLast("encoder",new StringEncoder());
+//                //
+//               pipeline.addLast("hoggenT",new HoggenTHandler());
+//                return pipeline;
+//            }
+//        });
+//
+//
+//        bootstrap.bind(new InetSocketAddress(8099));
+//
+//        System.out.println("start");
+//
+//    }
+//}
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
